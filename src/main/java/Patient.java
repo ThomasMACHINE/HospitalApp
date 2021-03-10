1e42899e309e94d6139bbbab3bde34107196fa12
ThomasMACHINE <53544690+ThomasMACHINE@users.noreply.github.com>
2021-03-10 06:29:19 +0100

    Accessor and Mutator methods implemented for task 1

--- src/main/java/Patient.java
+++ src/main/java/Patient.java
@@ -1,3 +1,20 @@
-public class Patient {
+public class Patient extends Person{
     private String diagnosis;
+    /**
+     * @param firstName - first name of Person
+     * @param lastName - last name of Person
+     * @param socialSecurityNumber - Unique ID of Person (Don't know why a social security number is in string)
+     */
+    public Patient(String firstName, String lastName, String socialSecurityNumber)
+    {
+        super(firstName, lastName, socialSecurityNumber);
+    }
+
+    /**
+     * gets diagnosis of patient
+      * @return diagnosis of patient
+     */
+    public String getDiagnose() {
+        return diagnosis;
+    }
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -10,8 +10,72 @@ public abstract class Person {
      */
     public Person(String firstName, String lastName, String socialSecurityNumber)
     {
-        private this.firstName = firstName;
-        private this.lastName = lastName;
-        private this.socialSecurityNumber = socialSecurityNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socialSecurityNumber = socialSecurityNumber;
     }
+
+    /**
+     * For better flow and readability "Personnummer" should be replaced with socialSecurityNumber
+     * Sends socialSecurityNumber of Person
+     * @return socialSecurityNumber - socialSecurityNumber of person
+     */
+    public String getPersonnummer(){
+        return this.socialSecurityNumber;
+    }
+
+    /**
+     * Attempts to set person number, should check if anyone has this personNumber
+     * as it is supposed to be uniqe
+     * @param attemptedPersonnummer - the social securty number they want to change to
+     */
+    public void setPersonnummer(String attemptedPersonnummer)
+    {
+        this.socialSecurityNumber = attemptedPersonnummer;
+    }
+
+    /**
+     * Obvious typo in class model, but I am going to follow it
+     * to avoid punishment!!!!!
+     * Names are not supposed to be unique so no need for duplication checks
+     * @param newFirstName - sets firstName to newFirstName
+     */
+    public void setFirtName(String newFirstName)
+    {
+        this.firstName = newFirstName;
+    }
+
+    /**
+     * Sets last name of Person object
+     * Names are not supposed to be unique, so no need for duplication checks
+     * @param newLastName - sets lastName to newLastName
+     */
+    public void setLastName(String newLastName) {
+        this.lastName = newLastName;
+    }
+
+    /**
+     * returns first name of Person object
+     * @return - first name
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * returns last name of Person object
+     * @return - last name
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Returns full name of person
+     * @return - concatenated string of firstName and lastName fields
+     */
+    public String getFullNavn() {
+        return firstName + " " + lastName;
+    }
+    //Add toString method
 }
